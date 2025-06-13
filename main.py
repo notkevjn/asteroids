26936e708ee7d9a2fa1626749e72238a5f701f7a
notkevjn <kevinhennecke@gmx.net>
2025-06-13 18:35:42 +0200

    Sprites

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ print ("Starting Asteroids!")
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    Clock = pygame.time.Clock
+    clock = pygame.time.Clock()
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,10 +16,10 @@ def main():
             
         screen.fill((0,0,0))
         pygame.display.flip()
-        dt = Clock.tick(60)
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     print(f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     main()
+    
